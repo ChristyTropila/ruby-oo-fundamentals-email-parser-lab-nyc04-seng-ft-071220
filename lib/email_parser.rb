c4aa9d9e85a28e4980a638241571793d3b7ba3f0
Christy Tropila <ctropilalakebeverage@gmail.com>
2020-08-03 21:04:36 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,9 +4,13 @@
 # or whitespace (' ').
 
 class EmailAddressParser
-  attr_accessor :email_addresses
+  attr_reader :emails
 
+  def initialize(emails)
+    @emails = emails
+  end
 
-
-
+  def parse
+    emails.split(/, | /).uniq
+  end
 end
